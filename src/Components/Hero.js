6baf39101335f8960ba6613c14fa588d465e2718
zarrar79 <shahzarrar79@gmail.com>
2024-01-28 16:48:55 +0500

    hero section desktop

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,8 @@
 import Logo from './hoo.png'
 import Discount from './Discount.png'
 import Arrow from './Arrow.png'
+import AI from './AI.png'
+import Line from './Line.png'
 export const Hero = () => {
     const display = () => {
         const menuElements = document.getElementsByClassName('menuNew');
@@ -37,33 +39,22 @@ export const Hero = () => {
                             {/* components */}
                             <div className='r_compo font-[400] text-[16px] text-[#FFFFFFB2] flex space-x-16'>
                                 {/* texts */}
-                                <div>Home</div>
-                                <div>About Us</div>
-                                <div>Features</div>
-                                <div>Solution</div>
+                                <div className='cursor-pointer'>Home</div>
+                                <div className='cursor-pointer'>About Us</div>
+                                <div className='cursor-pointer'>Features</div>
+                                <div className='cursor-pointer'>Solution</div>
                             </div>
                             {/* Hamburger */}
                             <div id="menuToggle" onClick={display}>
-                                {/* <!--
-      A fake / hidden checkbox is used as click reciever,
-      so you can use the :checked selector on it.
-      --> */}
+
                                 <input type="checkbox" />
 
-                                {/* <!--
-      Some spans to act as a hamburger.
-      
-      They are acting like a real hamburger,
-      not that McDonalds stuff.
-      --> */}
+
                                 <span></span>
                                 <span></span>
                                 <span></span>
 
-                                {/* <!--
-      Too bad the menu has to be inside of the button
-      but hey, it's pure CSS magic.
-      --> */}
+
                                 <ul id="menu">
                                     <a href="#"><li>Home</li></a>
                                     <a href="#"><li>About</li></a>
@@ -83,41 +74,70 @@ export const Hero = () => {
                     </div>
                 </div>
             </div>
-            {/* text +image +mx-width */}
             {/* fluid container */}
-            <div className='background'>
-                {/* image + text */}
-                <div className='grid grid-cols-2 font-Poppins'>
-                    {/* text */}
-                    <div>
-                        {/* strip */}
-                        <div className='background_1 flex max-w-[408px] p-[0.3rem] items-center rounded-md space-x-3'>
-                            {/* image */}
-                            <div ><img src={Discount}></img></div>
-                            {/* text */}
-                            <div className='font-Poppins text-[18px] font-[400] text-[#FFFFFF99]'><span className='text-[#ffff]'>20%</span> DISCOUNT FOR <span className='text-[#ffff]'>1 MONTH</span> ACCOUNT</div>
-                        </div>
-                        {/* b-text and round */}
-                        <div>
-                            {/* b-text */}
-                            <div className='text-[72px] font-[600] text-[#ffff]'>
-                                The Next <span className='background_3'>Generation</span> Payment Method.
-                            </div>
-                            {/* round + img */}
-                            <div className='p-8 flex justify-center border-[#49d0dc] relative rounded-full border-[1.5px] max-w-[140px] h-[140px]'>
-                                {/*round  */}
-                                <div className='flex justify-center border-[] text-[18px] font-[500] background_4 mt-3'>Get<br></br>Started</div>
-                                {/* img */}
-                                <div className='left-16 top-[42px] absolute text-center'><img src={Arrow}></img></div>
-                            </div>
-                        </div>
+            <div className='background pt-[0.1px]' >
+                {/* text +image +mx-width */}
 
+                <div className='mx-auto max-w-[1050px] large'>
+                    {/* image + text */}
+                    <div className='font-Poppin mt-5'>
+                        {/* text */}
+                        <div className='mt-[105px]'>
+                            {/* strip */}
+                            <div className='mb-3 background_1 flex max-w-[408px] p-[0.3rem] items-center rounded-md space-x-3'>
+                                {/* image */}
+                                <div ><img src={Discount}></img></div>
+                                {/* text */}
+                                <div className='font-Poppins text-[18px] font-[400] text-[#FFFFFF99]'><span className='text-[#ffff]'>20%</span> DISCOUNT FOR <span className='text-[#ffff]'>1 MONTH</span> ACCOUNT</div>
+                            </div>
+                            {/* b-text and round */}
+                            <div className='flex relative'>
+                                {/* b-text */}
+                                <div className='text-[72px] font-[600] text-[#ffff] leading-[100.8px]'>
+                                    The Next<br></br> <span className='background_3'>Generation</span><br></br> Payment Method.
+                                </div>
+                                {/* wrap absolute */}
+                                <div className='absolute left-[28rem] top-2'>
+                                    {/* round + img */}
+                                    <div className='p-8 flex justify-center border-[#49d0dc] relative rounded-full border-[1.5px] max-w-[200px] h-[130px]'>
+                                        {/*round  */}
+                                        <div className='flex justify-center text-[18px] font-[500] background_4 mt-1'>Get<br></br>Started</div>
+                                        {/* img */}
+                                        <div className='left-[3.7rem] top-[34px] absolute text-center'><img src={Arrow}></img></div>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* s-text */}
+                            <div className='mt-8 font-[400] text-[18px] leading-[30.6px] text-[#FFFFFFB2]'>Our team of experts uses a methodology to identify<br></br> the credit cards most likely to fit your needs.<br></br>
+                                We examine annual percentage rates, annual fees.</div>
+
+                        </div>
+                        {/* image */}
+                        <div className='absolute top-[8rem] right-0'><img className='max-w-[550px]' src={AI}></img></div>
                     </div>
-                    {/* image */}
-                    <div></div>
+                     {/* footer of hero */}
+                <div className='mt-[8rem] font-Poppins flex justify-between items-center'>
+                    {/* amount */}
+                    <div className='font-[600] text-[40.89px] text-[#ffff]'>3800+</div>
+                    {/* text */}
+                    <div className='font-[400] text-[20.45px] background_3'>User Active</div>
+                    {/* line */}
+                    <div><img src={Line}></img></div>
+                    {/* amount */}
+                    <div className='font-[600] text-[40.89px] text-[#ffff]'>230+</div>
+                    {/* text */}
+                    <div className='font-[400] text-[20.45px] background_3'>TRUSTED BY COMPANY</div>
+                    {/* line */}
+                    <div><img src={Line}></img></div>
+                    {/* amount */}
+                    <div className='font-[600] text-[40.89px] text-[#ffff]'>$230M+</div>
+                    {/* text */}
+                    <div className='font-[400] text-[20.45px] background_3'>TRANSACTION</div>
                 </div>
             </div>
-            <div></div>
+                </div>
+               
+
 
         </>
     )
